20a91f0fa3361649ff82d7e3e5a5e463082cffdc
David Jelley <djelleyjr@gmail.com>
2014-02-07 15:19:03 -0500

    Initial Setup on Local System
    Set up eclipse and linked into the github branch.

--- src/www/index.php
+++ src/www/index.php
@@ -1,9 +1,24 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+      <!--
+        University of Massachusetts Lowell
+        GUI Programming II, Prof. Jesse Heines
+		
+        Senior Project: Last Resort Recovery
+        Authors - David Jelley, Jr.
+                  Cameron Morris
+                  Benjamin Cao
+
+        Description: This semester we are tasked to take the knowledge we learned in GUI I and spend 
+                     the entire semester developing an interesting, advanced, and professional webpage.
+
+        Version 0.01
+      -->
+      
 <?php
 
-include 'LRR/site.php';
 
-$site = new LastResortRecovery();
-
-$site->renderHead();
 
 ?>
+
