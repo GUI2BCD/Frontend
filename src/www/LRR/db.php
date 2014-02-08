de35d80f4599166bc72ba07f25f61549f3775bb1
Cameron Morris <zephyrz2712@gmail.com>
2014-02-07 22:45:39 -0500

    Eclipse formatting is evil :D

--- src/www/LRR/db.php
+++ src/www/LRR/db.php
@@ -5,8 +5,8 @@
  * 
  * Last Resort Recovery
  * Authors - David Jelley, Jr.
- * 					 Cameron Morris
- * 					 Benjamin Cao
+ *           Cameron Morris
+ *           Benjamin Cao
  * 
  * Description: This file contains all the functionality related to interacting
  *              with the database.
@@ -24,61 +24,61 @@ $dbname = "";
  * configured for it
  *
  * @param string $host
- *        	Hostname of MySQL server
+ *          Hostname of MySQL server
  * @param integer $port
- *        	Port number of MySQL server
+ *          Port number of MySQL server
  * @param string $user
- *        	User with access to database
+ *          User with access to database
  * @param string $password
- *        	Password for user
+ *          Password for user
  * @param string $dbname
- *        	Name of the database to connect to
+ *          Name of the database to connect to
  * @return object MySQL Connection object which is connected to database
  */
 function mysqlConnect($host, $port, $user, $password, $dbname) {
-	
-	// Not default port
-	if ($port != 3306) {
-		$connection = mysqli_connect ( $host . ":" . $port, $user, $password );
-	} else {
-		$connection = mysqli_connect ( $host, $user, $password );
-	}
-	
-	// Select database
-	if ($dbname != NULL) {
-		mysqli_select_db ( $connection, $dbname );
-	}
-	
-	// Error in connecting
-	if (mysqli_connect_errno ()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error ();
-		die ();
-	}
-	
-	return $connection;
+  
+  // Not default port
+  if ($port != 3306) {
+    $connection = mysqli_connect ( $host . ":" . $port, $user, $password );
+  } else {
+    $connection = mysqli_connect ( $host, $user, $password );
+  }
+  
+  // Select database
+  if ($dbname != NULL) {
+    mysqli_select_db ( $connection, $dbname );
+  }
+  
+  // Error in connecting
+  if (mysqli_connect_errno ()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error ();
+    die ();
+  }
+  
+  return $connection;
 }
 
 /**
  * This function creates the user table
  *
  * @param string $host
- *        	Hostname of MySQL server
+ *          Hostname of MySQL server
  * @param integer $port
- *        	Port number of MySQL server
+ *          Port number of MySQL server
  * @param string $user
- *        	User with access to database
+ *          User with access to database
  * @param string $password
- *        	Password for user
+ *          Password for user
  * @param string $dbname
- *        	Name of the database to connect to
+ *          Name of the database to connect to
  */
 function createUsersTable($host, $port, $user, $password, $dbname) {
-	
-	// Connect to mysql server
-	$connection = mysqlConnect ( $host, $port, $user, $password, $dbname );
-	
-	// SQL Expression to create users table
-	$sql = "CREATE TABLE users
+  
+  // Connect to mysql server
+  $connection = mysqlConnect ( $host, $port, $user, $password, $dbname );
+  
+  // SQL Expression to create users table
+  $sql = "CREATE TABLE users
 	(
 	ID INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(ID),
@@ -86,55 +86,55 @@ function createUsersTable($host, $port, $user, $password, $dbname) {
 	password BLOB,
 	email TINYBLOB
 	);";
-	
-	// Execute query
-	if (mysqli_query ( $connection, $sql )) {
-		echo "Table users created successfully.<br>";
-	} else {
-		echo "Error creating table: " . mysqli_error ( $connection ) . "<br>";
-	}
-	
-	// Close connection
-	mysqli_close ( $connection );
+  
+  // Execute query
+  if (mysqli_query ( $connection, $sql )) {
+    echo "Table users created successfully.<br>";
+  } else {
+    echo "Error creating table: " . mysqli_error ( $connection ) . "<br>";
+  }
+  
+  // Close connection
+  mysqli_close ( $connection );
 }
 
 /**
  * This function creates the devices table
  *
  * @param string $host
- *        	Hostname of MySQL server
+ *          Hostname of MySQL server
  * @param integer $port
- *        	Port number of MySQL server
+ *          Port number of MySQL server
  * @param string $user
- *        	User with access to database
+ *          User with access to database
  * @param string $password
- *        	Password for user
+ *          Password for user
  * @param string $dbname
- *        	Name of the database to connect to
+ *          Name of the database to connect to
  */
 function createDevicesTable($host, $port, $user, $password, $dbname) {
-	
-	// Connect to mysql server
-	$connection = mysqlConnect ( $host, $port, $user, $password, $dbname );
-	
-	// SQL Expression to create devices table
-	$sql = "CREATE TABLE devices
+  
+  // Connect to mysql server
+  $connection = mysqlConnect ( $host, $port, $user, $password, $dbname );
+  
+  // SQL Expression to create devices table
+  $sql = "CREATE TABLE devices
 	(
 	ID INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(ID),
 	userid INT NOT NULL,
 	name TINYBLOB
 	);";
-	
-	// Execute query
-	if (mysqli_query ( $connection, $sql )) {
-		echo "Table devices created successfully.<br>";
-	} else {
-		echo "Error creating table: " . mysqli_error ( $connection ) . "<br>";
-	}
-	
-	// Close connection
-	mysqli_close ( $connection );
+  
+  // Execute query
+  if (mysqli_query ( $connection, $sql )) {
+    echo "Table devices created successfully.<br>";
+  } else {
+    echo "Error creating table: " . mysqli_error ( $connection ) . "<br>";
+  }
+  
+  // Close connection
+  mysqli_close ( $connection );
 }
 
 ?>
