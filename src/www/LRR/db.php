c9b5edd2573dafa15a492a7abd8df024979a2f59
Cameron Morris <zephyrz2712@gmail.com>
2014-02-08 17:49:50 -0500

    Added function to create a MySQL database

--- src/www/LRR/db.php
+++ src/www/LRR/db.php
@@ -59,6 +59,41 @@ function mysqlConnect($host, $port, $user, $password, $dbname) {
 }
 
 /**
+ * This function will create a MySQL database with the given name
+ * 
+ * @param string $host
+ *          Hostname of MySQL server
+ * @param integer $port
+ *          Port number of MySQL server
+ * @param string $user
+ *          User with access to MySQL server
+ * @param string $password
+ *          Password for user
+ * @param string $dbname
+ *          Name of the database to create
+ */
+function createDatabase( $host, $port, $user, $password, $dbname) {
+
+  // Connect to mysql server
+  $connection = mysqlConnect( $host, $port, $user, $password, NULL);
+
+  // Create database
+  $sql = "CREATE DATABASE " . $dbname . ";";
+
+  // Execute query
+  if( mysqli_query( $connection, $sql )) {
+    echo "Database " . $dbname . " created successfully.<br>";
+  }
+  else {
+    echo "Error creating database: " . mysqli_error($connection) . "<br>";
+  }
+
+  // Close connection
+  mysqli_close($connection);
+}
+
+
+/**
  * This function creates the user table
  *
  * @param string $host
