3db9199012eeb25be3cc045d4565d1f6404bbec0
David Jelley <djelleyjr@gmail.com>
2014-02-07 22:55:53 -0500

    First commit
    I setup the template for our webpage and added the basic input fields
    for the login process. I made a shitty ass logo and figured out how to
    arrange the service bar so it is fixed at the top of the page. I also
    created the php file for the service bar. This should contain the
    scripts for getting the values of the input fields and checking them
    with the user's accounts in the database.

--- src/www/index.php
+++ src/www/index.php
@@ -1,3 +1,5 @@
+<?php /*Include .PHP files here. */ ?>
+
 <html>
   <head>
     <meta charset="UTF-8">
@@ -15,10 +17,35 @@
 
         Version 0.01
       -->
+
+      <!-- Link stylesheets here. -->
+      <link rel="stylesheet" type="text/css" media="all" href="./css/template.css" />
+      <link rel="stylesheet" type="text/css" media="all" href="./css/servicebar.css" />
       
-<?php
-
-
-
-?>
-
+  </head>
+  <body class="homepage">
+  
+    <div id="service">
+     	<div id="service-content">
+     		<div id="service-left">
+     		  <img src="./images/icon.png" alt="Icon">
+     		</div>
+     		<div id="service-right">
+	     		<input class="service-login" type="text" placeholder="username" form="login" required="required">
+  	   		<input class="service-login" type="password" placeholder="password" form="login" required="required">
+  	   		<button type="submit" form="login">Log In</button>
+  	   		<a href="#">Sign-up</a>
+     		</div>
+     	</div>
+    </div>
+    
+    <div id="wrapper">
+      <div id="header">
+      </div>
+      <div id="content">
+      </div>
+      <div id="footer">
+      </div>
+    </div>
+  </body>
+</html>
