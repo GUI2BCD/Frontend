2783ff25561c609f224de00f0e06740803ac31e7
Cameron Morris <zephyrz2712@gmail.com>
2014-02-09 15:43:39 -0500

    Mass reformatting of conflicting files

--- src/www/index.php
+++ src/www/index.php
@@ -1,11 +1,12 @@
-<?php /*Include .PHP files here. */ 
+<?php 
+/* Include .PHP files here. */
 include './LRR/servicebar.php';
 ?>
 
 <html>
-  <head>
-    <meta charset="UTF-8">
-      <!--
+<head>
+<meta charset="UTF-8">
+<!--
         University of Massachusetts Lowell
         GUI Programming II, Prof. Jesse Heines
 		
@@ -20,59 +21,79 @@ include './LRR/servicebar.php';
         Version 0.01
       -->
 
-      <!-- Link local stylesheets and jQuery here. -->
-      <link rel="stylesheet" type="text/css" media="all" href="./css/template.css" />
-      <link rel="stylesheet" type="text/css" media="all" href="./css/public.css" />
-      <link rel="stylesheet" type="text/css" media="all" href="./css/servicebar.css" />
+<!-- Link local stylesheets and jQuery here. -->
+<link rel="stylesheet" type="text/css" media="all"
+  href="./css/template.css" />
+<link rel="stylesheet" type="text/css" media="all"
+  href="./css/public.css" />
+<link rel="stylesheet" type="text/css" media="all"
+  href="./css/servicebar.css" />
 
-      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-      <script src="./js/login.js"></script>
-      
-      <!-- Link jQuery UI here. -->
-      <link rel="stylesheet" type="text/css" media="all" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />
-      <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-      
-  </head>
-  <body class="homepage">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="./js/login.js"></script>
+
+<!-- Link jQuery UI here. -->
+<link rel="stylesheet" type="text/css" media="all"
+  href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />
+<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+</head>
+<body class="homepage">
   <!-- Dialog Box Div -->
   <div id="login-prompt" title="Sign in">
     <form>
-        <fieldset>
-            <label for="login-username">Username:</label><br />
-            <input type="text" name="login-username" id="login-username" class="text ui-widget-content ui-corner-all"><br />
-            <label for="login-password">Password:</label><br />
-            <input type="password" name="login-password" id="login-password" class="text ui-widget-content ui-corner-all">
-        </fieldset>
+      <fieldset>
+        <label for="login-username">Username:</label><br /> <input
+          type="text" name="login-username" id="login-username"
+          class="text ui-widget-content ui-corner-all"><br /> <label
+          for="login-password">Password:</label><br /> <input
+          type="password" name="login-password" id="login-password"
+          class="text ui-widget-content ui-corner-all">
+      </fieldset>
     </form>
   </div>
-  
-    <div id="service">
-     	<div id="service-content">
+
+  <div id="service">
+    <div id="service-content">
      	  <?php $servicebar = new servicebar(); ?>
      	</div>
+  </div>
+  <div id="wrapper">
+    <div id="header">
+      <form>
+        <p>
+          <input type="text" name="username" id="signup-username"
+            placeholder="Pick a username"
+            class="text ui-widget-content ui-corner-all">
+        </p>
+        <p>
+          <input type="text" name="email" id="signup-email"
+            placeholder="Your email"
+            class="text ui-widget-content ui-corner-all">
+        </p>
+        <p>
+          <input type="password" name="password" id="signup-password"
+            placeholder="Create a password"
+            class="text ui-widget-content ui-corner-all">
+        </p>
+        <p>
+          <input type="password" name="password2" id="signup-password2"
+            placeholder="Verify your password"
+            class="text ui-widget-content ui-corner-all">
+        </p>
+        <p>
+          <input type="button" name="newuser-submit" id="signup-submit"
+            value="Sign up now!">
+        </p>
+      </form>
     </div>
-    <div id="wrapper">
-      <div id="header">
-        <form>
-          <p><input type="text" name="username" id="signup-username" placeholder="Pick a username" class="text ui-widget-content ui-corner-all"></p>
-          <p><input type="text" name="email" id="signup-email" placeholder="Your email" class="text ui-widget-content ui-corner-all"></p>
-          <p><input type="password" name="password" id="signup-password" placeholder="Create a password" class="text ui-widget-content ui-corner-all"></p>
-          <p><input type="password" name="password2" id="signup-password2" placeholder="Verify your password" class="text ui-widget-content ui-corner-all"></p>
-          <p><input type="button" name="newuser-submit" id="signup-submit" value="Sign up now!"></p>
-        </form>
-      </div>
-      <div id="content">
-        <div class="content-image-left">
-        </div>
-        <div class="content-image-right">
-        </div>
-        <div class="content-image-left">
-        </div>
-        <div class="content-image-right">
-        </div>
-      </div>
-      <div id="footer">
-      </div>
+    <div id="content">
+      <div class="content-image-left"></div>
+      <div class="content-image-right"></div>
+      <div class="content-image-left"></div>
+      <div class="content-image-right"></div>
     </div>
-  </body>
+    <div id="footer"></div>
+  </div>
+</body>
 </html>
