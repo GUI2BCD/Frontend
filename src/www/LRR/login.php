77a5b2d554520a45f1952acab19232f929cc5aba
Cameron Morris <zephyrz2712@gmail.com>
2014-02-09 17:07:13 -0500

    Added login processing page

--- src/www/LRR/login.php
+++ src/www/LRR/login.php
@@ -10,3 +10,31 @@
  *
  * Description: This file handles the login/registration process.
  */
+namespace LastResortRecovery;
+
+include_once 'db.php';
+include_once 'session.php';
+
+// Start a session
+Session::startSecureSession();
+
+// Check post variables
+if( isset( $_POST['email'], $_POST['password']) ) {
+ 
+    $email = $_POST['email'];
+    // Encrypted password
+    $password = $_POST['password'];
+    
+    // Check if login valid
+    if( Session::login($email, $password, $connection) ) {
+        // Login success
+        echo 'Login success';
+    }
+    else {
+        // Login failed
+        echo 'Login failed';
+    }
+    
+} else {
+    // Nothing was posted
+}
