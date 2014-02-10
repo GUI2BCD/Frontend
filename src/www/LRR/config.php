c024cd59e4263fe4e1ebfbd3b442d0370396a518
Cameron Morris <zephyrz2712@gmail.com>
2014-02-10 16:33:51 -0500

    Updated to hold error messages

--- src/www/LRR/config.php
+++ src/www/LRR/config.php
@@ -10,14 +10,33 @@
  *
  * Description: This file will hold the global configuration variables
  */
+namespace LastResortRecovery
 
-// Hostname of the MySQL server
-define("HOST", "localhost");
-// Port number of MySQL server
-define("PORT", 3306);
-// Database username
-define("USER", "root");
-// Database password
-define("PASSWORD", "");
-// Name of MySQL database
-define("DATABASE", "lastresortrecovery");
+{
+    
+    // Hostname of the MySQL server
+    define("HOST", "localhost");
+    // Port number of MySQL server
+    define("PORT", 3306);
+    // Database username
+    define("USER", "root");
+    // Database password
+    define("PASSWORD", "");
+    // Name of MySQL database
+    define("DATABASE", "lastresortrecovery");
+    
+    // Error messages
+    define("LOGIN_SUCCESS", "Logged in");
+    define("LOGIN_PASSWORD", "Bad password");
+    define("LOGIN_USER", "User not found");
+    define("LOGIN_EMAIL", "Email not found");
+    define("LOGIN_SESSION", "Invalid session");
+
+    define("REGISTER_SUCCESS", "Registered");
+    define("REGISTER_EMAIL", "Email in use");
+    define("REGISTER_USER", "Username is use");
+
+
+
+    define("DATABASE_ERROR", "Database error");
+}
