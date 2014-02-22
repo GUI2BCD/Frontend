e054b7790327e1caa337c3ea3c95fbe90ba8582f
David Jelley <djelleyjr@gmail.com>
2014-02-22 14:37:03 -0500

    Added basic tab functionality

--- src/www/dashboard.php
+++ src/www/dashboard.php
@@ -26,10 +26,10 @@ if (! Session::loginCheck($connection)) {
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet"
-	href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <!-- Optional theme -->
 <link rel="stylesheet"
-	href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+    href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
 <!-- Custom styles for this template -->
 <link href="css/jumbotron.css" rel="stylesheet">
@@ -46,40 +46,80 @@ if (! Session::loginCheck($connection)) {
 
 <body>
 
-	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse"
-					data-target=".navbar-collapse">
-					<span class="sr-only">Toggle navigation</span> <span
-						class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Last Resort Recovery</a>
-			</div>
-			<div class="navbar-collapse collapse"></div>
-			<!--/.navbar-collapse -->
-		</div>
-	</div>
+    <div class="navbar navbar-inverse navbar-fixed-top"
+        role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle"
+                    data-toggle="collapse"
+                    data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span> <span
+                        class="icon-bar"></span> <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Last Resort Recovery</a>
+            </div>
+            <div class="navbar-right">
+                <a class="navbar-brand">Welcome, username!</a>
+            </div>
+            <div class="navbar-collapse collapse"></div>
+            <!--/.navbar-collapse -->
+        </div>
+    </div>
 
-	<div class="container content">
+    <div class="container content">
 
-    
-		
-		
-		
-		<!-- FOOTER -->
-		<hr>
-		<footer>
-			<p class="pull-right">
-				<a href="#">Back to top</a>
-			</p>
-			<p>
-				&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot;
-				<a href="#">Terms</a>
-			</p>
-		</footer>
+<!-- Nav tabs -->
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#dashboard" data-toggle="tab">Dashboard</a></li>
+  <li><a href="#devices" data-toggle="tab">Devices</a></li>
+  <li><a href="#agent" data-toggle="tab">Agent</a></li>
+  <li><a href="#account" data-toggle="tab">Account</a></li>
+</ul>
 
-	</div>
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="dashboard">
+  
+  <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Account</h3>
+  </div>
+  <div class="panel-body">
+    Name: <br /><br />
+    Username:
+  </div>
+</div>
+  
+  </div>
+  <div class="tab-pane" id="devices">test2</div>
+  <div class="tab-pane" id="agent">3</div>
+  <div class="tab-pane" id="account">and 4</div>
+</div>
+
+
+
+        <!-- FOOTER -->
+        <hr>
+        <footer>
+            <p class="pull-right">
+                <a href="#">Back to top</a>
+            </p>
+            <p>
+                &copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a>
+                &middot; <a href="#">Terms</a>
+            </p>
+        </footer>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script
+        src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
