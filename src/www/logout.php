9fae04adde7eb3a44bf7b9f918303c23cd902d3e
Cameron Morris <zephyrz2712@gmail.com>
2014-02-26 15:25:27 -0500

    Formatting

--- src/www/logout.php
+++ src/www/logout.php
@@ -28,10 +28,10 @@ $params = session_get_cookie_params();
 setcookie(
     session_name(),
     '', 
-    time() - 42000, 
-    $params["path"], 
-    $params["domain"], 
-    $params["secure"], 
+    time() - 42000,
+    $params["path"],
+    $params["domain"],
+    $params["secure"],
     $params["httponly"]
 );
 
