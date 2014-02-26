22727d1403e871b544ade144b357167cb3dd6b65
Cameron Morris <zephyrz2712@gmail.com>
2014-02-26 15:20:29 -0500

    Added comment

--- src/www/logout.php
+++ src/www/logout.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * University of Massachusetts Lowell
+ * GUI Programming II, Prof. Jesse Heines
+ *
+ * Last Resort Recovery
+ * Authors - David Jelley, Jr.
+ *           Cameron Morris
+ *           Benjamin Cao
+ *
+ * Description: This file handles the logout process
+ *
+ */
+
 namespace LastResortRecovery;
 
 include_once 'db.php';
