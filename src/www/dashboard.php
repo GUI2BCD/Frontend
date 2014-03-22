cb75044145d39913fa88d75651d31d211f9eebf6
Cameron Morris <zephyrz2712@gmail.com>
2014-03-21 20:14:27 -0400

    Style fixes

--- src/www/dashboard.php
+++ src/www/dashboard.php
@@ -256,7 +256,10 @@ while ($row = mysqli_fetch_array($result)) {
     } else {
         echo '<h4 class="status-red">';
     }
-    echo $row['status'] . '</h4> <br><button device-id="' . $row['id'] . '" type="button" class="toggleStatusButton btn btn-danger btn-xs">Toggle status</button>';
+    echo $row['status'] .
+    '</h4> <br><button device-id="' .
+    $row['id'] .
+    '" type="button" class="toggleStatusButton btn btn-danger btn-xs">Toggle status</button>';
     ?>
                                 </div>
                                 
--- src/www/js/common.js
+++ src/www/js/common.js
@@ -4,22 +4,23 @@
  */
 
 /**
- * Toggles all spans(chevrons) from down to up when an element with 
- * the class "accordion-icon-swap" is clicked.
+ * Toggles all spans(chevrons) from down to up when an element with the class
+ * "accordion-icon-swap" is clicked.
  */
 $(".accordion-icon-swap").click(
         function() {
-            $(this).find('.glyphicon').toggleClass('glyphicon-chevron-down').toggleClass(
-                    'glyphicon-chevron-up');
+            $(this).find('.glyphicon').toggleClass('glyphicon-chevron-down')
+                    .toggleClass('glyphicon-chevron-up');
         });
 
-/**************************************************************************/
-/** The following group of functions will make sure the browser displays **/
-/** the tab that the user was on before a browser refresh.               **/
-/**                                                                      **/
-/** Source:
-http://stackoverflow.com/questions/18999501/bootstrap-3-keep-selected-tab-on-page-refresh
-/**************************************************************************/
+/** *********************************************************************** */
+/** The following group of functions will make sure the browser displays * */
+/** the tab that the user was on before a browser refresh. * */
+/** * */
+/*******************************************************************************
+ * Source:
+ * http://stackoverflow.com/questions/18999501/bootstrap-3-keep-selected-tab-on-page-refresh /
+ ******************************************************************************/
 /**
  * Prevents the rendering of the default tab.
  */
@@ -31,16 +32,16 @@ $('user-tabs').click(function(e) {
 /**
  * Stores the currently selected tab in the hash value. (First Tier)
  */
-$("ul.nav-tabs > li > a").on("show.bs.tab", function (e) {
+$("ul.nav-tabs > li > a").on("show.bs.tab", function(e) {
     var id = $(e.target).attr("href").substr(1);
     window.location.hash = id;
 });
 
 /**
- * Stores the currently selected tab in the hash value. (Second Tier)
- *      - I had to add this to accomadate my drop-down menu.
+ * Stores the currently selected tab in the hash value. (Second Tier) - I had to
+ * add this to accomadate my drop-down menu.
  */
-$("ul.nav-tabs > li > ul > li > a").on("show.bs.tab", function (e) {
+$("ul.nav-tabs > li > ul > li > a").on("show.bs.tab", function(e) {
     var id = $(e.target).attr("href").substr(1);
     window.location.hash = id;
 });
@@ -49,18 +50,16 @@ $("ul.nav-tabs > li > ul > li > a").on("show.bs.tab", function (e) {
  * On page load - switch to currently selected tab
  */
 var hash = window.location.hash;
-$('#user-tabs a[href="'+ hash + '"]').tab('show');
+$('#user-tabs a[href="' + hash + '"]').tab('show');
 
-$(".toggleStatusButton").click(
-	function(){
-	        $.post(
-	            "ajax.php",
-	            {action: "togglestatus", deviceid: $( this ).attr('device-id') },
-	            function(responseText){
-	                console.log(responseText);
-	            },
-	            "html"
-	        );
-	});
-
-
+/**
+ * Handles ajax call to toggle device status
+ */
+$(".toggleStatusButton").click(function() {
+    $.post("ajax.php", {
+        action : "togglestatus",
+        deviceid : $(this).attr('device-id')
+    }, function(responseText) {
+        console.log(responseText);
+    }, "html");
+});
