5b44edccaf995983029b798e5bf2646c62a8a27a
David Jelley <djelleyjr@gmail.com>
2014-04-06 14:01:59 -0400

    Fixed page history
    Remembers which page you were on during refresh

--- src/www/js/common.js
+++ src/www/js/common.js
@@ -31,7 +31,7 @@ $('user-tabs').click(function(e) {
 /**
  * Stores the currently selected tab in the hash value. (First Tier)
  */
-$("ul.nav-tabs > li > a").on("show.bs.tab", function (e) {
+$("ul.nav-pills > li > a").on("show.bs.tab", function (e) {
     var id = $(e.target).attr("href").substr(1);
     window.location.hash = id;
 });
@@ -41,6 +41,7 @@ $("ul.nav-tabs > li > a").on("show.bs.tab", function (e) {
  *      - I had to add this to accomadate my drop-down menu.
  */
 $("ul.nav-pills > li > ul > li > a").on("show.bs.tab", function (e) {
+    console.log("Here");
     var id = $(e.target).attr("href").substr(1);
     window.location.hash = id;
 });
