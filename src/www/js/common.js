04d9c885de7667212affcb47ee3290a27178b086
Cameron Morris <zephyrz2712@gmail.com>
2014-04-15 10:12:25 -0400

    Fixed links

--- src/www/js/common.js
+++ src/www/js/common.js
@@ -57,7 +57,7 @@ $('#user-tabs a[href="'+ hash + '"]').tab('show');
 var offset = 60;
 
 $('a').click(function(event) {
-    event.preventDefault();
+//    event.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
 });
