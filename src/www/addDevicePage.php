2af41fa40ddb9ee300f0ea54ef2615d4dfab035a
Cameron Morris <zephyrz2712@gmail.com>
2014-04-21 22:25:38 -0400

    FIX THINGS

--- src/www/addDevicePage.php
+++ src/www/addDevicePage.php
@@ -28,7 +28,9 @@ namespace LastResortRecovery
         private function generatePage()
         {
             ?>
+
 <div class="tab-pane" id="help">
+<div class="page-space">
 	<div class="panel panel-default">
 		<div class="panel-heading">Setup Instructions</div>
 		<div id="rootwizard">
@@ -47,33 +49,58 @@ namespace LastResortRecovery
 			<div class="tab-content instructions">
 				<div class="tab-pane" id="tab1">
 					<div class="alert alert-info alerthelp">
-						<strong>Download</strong> the appropriate package for your operating system:
+						<strong>Download</strong> the appropriate package for your
+						operating system:
 					</div>
 					<div class="well well-lg alerthelp">
-					   <div class="row helprow">
-					       <div class="col-md-3">
-					           <div class="row"><img alt="Ubuntu/Debian" src="images/deb.svg"></div>
-					           <div class="row"><strong>Ubuntu/Debian</strong></div>
-					           <div class="row"><button type="button" class="btn btn-default">Download</button></div>
-					       </div>
-					       <div class="col-md-3">
-					           <div class="row"><img alt="ArchLinux" src="images/arch.svg"></div>
-					           <div class="row"><strong>Arch Linux</strong></div>
-					           <div class="row"><button type="button" class="btn btn-default">Download</button></div>
-					       </div>
-					       <div class="col-md-3">
-					           <div class="row"><img alt="osx" src="images/osx.svg"></div>
-					           <div class="row"><strong>Mac OSX</strong></div>
-					           <div class="row"><button type="button" class="btn btn-default">Coming Soon</button></div>
-					       </div>
-					       <div class="col-md-3">
-					           <div class="row"><img alt="Windows" src="images/win.svg"></div>
-					           <div class="row"><strong>Windows</strong></div>
-					           <div class="row"><button type="button" class="btn btn-default">Coming soon</button></div>
-					       </div>
-					   </div>
+						<div class="row helprow">
+							<div class="col-md-3">
+								<div class="row">
+									<img alt="Ubuntu/Debian" src="images/deb.svg">
+								</div>
+								<div class="row">
+									<strong>Ubuntu/Debian</strong>
+								</div>
+								<div class="row">
+									<button type="button" class="btn btn-default">Download</button>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="row">
+									<img alt="ArchLinux" src="images/arch.svg">
+								</div>
+								<div class="row">
+									<strong>Arch Linux</strong>
+								</div>
+								<div class="row">
+									<button type="button" class="btn btn-default">Download</button>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="row">
+									<img alt="osx" src="images/osx.svg">
+								</div>
+								<div class="row">
+									<strong>Mac OSX</strong>
+								</div>
+								<div class="row">
+									<button type="button" class="btn btn-default">Coming Soon</button>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="row">
+									<img alt="Windows" src="images/win.svg">
+								</div>
+								<div class="row">
+									<strong>Windows</strong>
+								</div>
+								<div class="row">
+									<button type="button" class="btn btn-default">Coming soon</button>
+								</div>
+							</div>
+						</div>
 					</div>
-					
+
 				</div>
 				<div class="tab-pane" id="tab2">
 					<div class="alert alert-info alerthelp">
@@ -81,53 +108,71 @@ namespace LastResortRecovery
 					</div>
 					<div class="well well-lg alerthelp">
 						<div class="row">
-						  <div class="col-md-2 helprow">
-						      <div class="row"><img alt="Ubuntu/Debian" src="images/deb.svg"></div>
-						      <div class="row"><strong>Ubuntu/Debian</strong></div>
-						  </div>
-						  <div class="col-md-10">
-						      <div class="row">Install with software center or through command line:</div>
-						      <div class="row"><kbd>sudo dpkg -i lastresortrecovery-0.2beta.deb</kbd></div>
-						      <div class="row">Resolve dependencies:</div>
-						      <div class="row"><kbd>sudo apt-get install -f</kbd></div>
-						  </div>
+							<div class="col-md-2 helprow">
+								<div class="row">
+									<img alt="Ubuntu/Debian" src="images/deb.svg">
+								</div>
+								<div class="row">
+									<strong>Ubuntu/Debian</strong>
+								</div>
+							</div>
+							<div class="col-md-10">
+								<div class="row">Install with software center or through command
+									line:</div>
+								<div class="row">
+									<kbd>sudo dpkg -i lastresortrecovery-0.2beta.deb</kbd>
+								</div>
+								<div class="row">Resolve dependencies:</div>
+								<div class="row">
+									<kbd>sudo apt-get install -f</kbd>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div class="well well-lg alerthelp">
 						<div class="row">
-						  <div class="col-md-2 helprow">
-						      <div class="row"><img alt="ArchLinux" src="images/arch.svg"></div>
-						      <div class="row"><strong>Arch Linux</strong></div>
-						  </div>
-						  <div class="col-md-10">
-						      <div class="row">Install through command line:</div>
-						      <div class="row"><kbd>sudo pacman -U lastresortrecovery-0.2beta-1-x86_64.pkg.tar.gz</kbd></div>
-						  </div>
-						
+							<div class="col-md-2 helprow">
+								<div class="row">
+									<img alt="ArchLinux" src="images/arch.svg">
+								</div>
+								<div class="row">
+									<strong>Arch Linux</strong>
+								</div>
+							</div>
+							<div class="col-md-10">
+								<div class="row">Install through command line:</div>
+								<div class="row">
+									<kbd>sudo pacman -U
+										lastresortrecovery-0.2beta-1-x86_64.pkg.tar.gz</kbd>
+								</div>
+							</div>
+
 						</div>
-						
+
 					</div>
 				</div>
 				<div class="tab-pane" id="tab3">
-				<div class="alert alert-info alerthelp" >
+					<div class="alert alert-info alerthelp">
 						<strong>Setup</strong> the agent:
-			    </div>
-			    <div class="well well-lg alerthelp">
-			    Start the agent with the following command:<br>
-			    <kbd>sudo lastresortagent</kbd><br>
-			    </div>
-			    <div class="well well-lg alerthelp">
-			    Follow the prompts to configure the agent with your account<br>
-			    </div>
-			    </div>
+					</div>
+					<div class="well well-lg alerthelp">
+						Start the agent with the following command:<br>
+						<kbd>sudo lastresortagent</kbd>
+						<br>
+					</div>
+					<div class="well well-lg alerthelp">
+						Follow the prompts to configure the agent with your account<br>
+					</div>
+				</div>
 				<div class="tab-pane" id="tab4">
-				<div class="alert alert-info alerthelp">
+					<div class="alert alert-info alerthelp">
 						<strong>Done!</strong>
-			    </div>
-			    <div class="well well-lg">
-			    All done! You're device is now ready. Select the device from the dashboard to view it.<br>
-			    </div>
-			    </div>
+					</div>
+					<div class="well well-lg">
+						All done! You're device is now ready. Select the device from the
+						dashboard to view it.<br>
+					</div>
+				</div>
 				<ul class="pager wizard">
 					<li class="previous first" style="display: none;"><a href="#">First</a></li>
 					<li class="previous"><a href="#">Previous</a></li>
@@ -137,6 +182,7 @@ namespace LastResortRecovery
 			</div>
 		</div>
 	</div>
+</div>
 </div>
 <?php
             return 1;
