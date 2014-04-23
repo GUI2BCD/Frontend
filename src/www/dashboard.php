6915259c5844057a05199102bcf4ac4e8f4e69c9
David Jelley <djelleyjr@gmail.com>
2014-04-22 21:58:47 -0400

    Submitting what I have.
    Work tomorrow, Cam see what you can do with that x-editable error I am
    getting. I will get back on it after work tomorrow.

--- src/www/dashboard.php
+++ src/www/dashboard.php
@@ -37,6 +37,12 @@ if (! Session::loginCheck($connection)) {
 <link rel="stylesheet"
     href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
+<!-- X-editable for AJAX and editable conent field. -->
+<!-- https://vitalets.github.io/x-editable/index.html -->
+<link
+    href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css"
+    rel="stylesheet" />
+
 <!-- Custom styles for this template -->
 <link href="css/jumbotron.css" rel="stylesheet">
 
@@ -177,9 +183,6 @@ while ($row = mysqli_fetch_array($result)) {
     <script src="./js/common.js"></script>
     <!-- X-editable for AJAX and editable conent field. -->
     <!-- https://vitalets.github.io/x-editable/index.html -->
-    <link
-        href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css"
-        rel="stylesheet" />
     <script
         src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
 
