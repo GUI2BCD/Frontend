37e202a54eefaa7958546b59b1f248234610a578
Cameron Morris <zephyrz2712@gmail.com>
2014-04-22 20:15:00 -0400

    test again

--- src/www/devicePage.php
+++ src/www/devicePage.php
@@ -211,13 +211,13 @@ namespace LastResortRecovery
                   center: new google.maps.LatLng(-34.397, 150.644),
                   zoom: 8
                 };
-                var map = new google.maps.Map(document.getElementById("map"),
+                var map = new google.maps.Map(document.getElementById("map-canvas"),
                     mapOptions);
               }
               google.maps.event.addDomListener(window, 'load', initialize);
             </script>
             <div class="panel-body clear-padding">
-            <div id="map"></div>
+            <div id="map-canvas" style="width: 100%; height: 100%"></div>
             </div>
         </div>
     </div>
