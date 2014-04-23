14ccbc5b12e93c92c11f049b7671c0826a136aa8
Cameron Morris <zephyrz2712@gmail.com>
2014-04-23 16:31:53 -0400

    Working editables

--- src/www/form.php
+++ src/www/form.php
@@ -1,40 +1,44 @@
 <?php
-    /*
-    Script for update record from X-editable.
-    */
+namespace LastResortRecovery;
 
-    //delay (for debug only)
-    sleep(1); 
+include_once 'db.php';
+include_once 'session.php';
+/*
+ * You will get 'pk', 'name' and 'value' in $_POST array.
+ */
+$pk = $_POST['pk'];
+$name = $_POST['name'];
+$value = $_POST['value'];
 
-    /*
-    You will get 'pk', 'name' and 'value' in $_POST array.
-    */
-    $pk = $_POST['pk'];
-    $name = $_POST['name'];
-    $value = $_POST['value'];
-
-    /*
-     Check submitted value
-    */
-    if(!empty($value)) {
-        /*
-          If value is correct you process it (for example, save to db).
-          In case of success your script should not return anything, standard HTTP response '200 OK' is enough.
-          
-          for example:
-          $result = mysql_query('update users set '.mysql_escape_string($name).'="'.mysql_escape_string($value).'" where user_id = "'.mysql_escape_string($pk).'"');
-        */
-        
-        //here, for debug reason we just return dump of $_POST, you will see result in browser console
-        print_r($_POST);
-
-
-    } else {
-        /* 
-        In case of incorrect value or error you should return HTTP status != 200. 
-        Response body will be shown as error message in editable form.
-        */
-
+/*
+ * Check submitted value
+ */
+if (! empty($value)) {
+    
+    if ($name == "username" && Session::checkUsername($value, $connection) != VALIDATION_OK) {
         header('HTTP 400 Bad Request', true, 400);
-        echo "This field is required!";
+        echo 'Username in use';
+    } elseif ($name == "email" && Session::checkEmail($value, $connection) != VALIDATION_OK) {
+        header('HTTP 400 Bad Request', true, 400);
+        echo 'Email in use';
+    } else {
+        $sql = "UPDATE users SET " . $name . " = ? WHERE id = ?;";
+        
+        // Prepare statement
+        if ($result = $connection->prepare($sql)) {
+            // Bind ID into query
+            $result->bind_param('si', $value, $pk);
+            // Execute query
+            echo $result->execute();
+        } else {
+            echo 'bad';
+        }
     }
+} else {
+    /*
+     * In case of incorrect value or error you should return HTTP status != 200. Response body will be shown as error message in editable form.
+     */
+    
+    header('HTTP 400 Bad Request', true, 400);
+    echo "This field is required!";
+}
